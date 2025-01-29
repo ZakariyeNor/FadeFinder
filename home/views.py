08dca6dee2fbdc2374602bbaa3109271da91aa77
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-29 15:38:21 +0000

    render barber description and barber services

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import BarberInfo
+from .models import BarberInfo, ServicesDes
 
 
 # Create your views here.
@@ -8,3 +8,17 @@ class BarberInfoContent(generic.ListView):
     model = BarberInfo
     template_name = 'home/home.html'
     context_object_name = 'barberinfo'
+
+
+    # def ServicesDescription(request):
+    #     servicedescription = ServicesDes.objects.all()
+    #     return render(
+    #         request,
+    #         'home/home.html',
+    #         {'servicedescription': servicedescription},
+    # )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['servicedescription'] = ServicesDes.objects.all()
+        return context
