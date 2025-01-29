d11f6a0dab1614d483d897818cbf3df717652fa7
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-29 15:09:24 +0000

    Add home page content

--- fadefinder/urls.py
+++ fadefinder/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home-urls'),
 ]
+
+
+
+
+# This will serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -1,15 +1,6 @@
 from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.BarberInfoContent.as_view(), name='home'),
 ]
-
-
-
-
-
-# This will serve media files in development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
