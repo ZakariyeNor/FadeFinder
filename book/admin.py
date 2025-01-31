02c65468284358567e5b337644cc3d722f81e269
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-31 13:09:11 +0000

    create cover model, add into summernote fields and import in the view file

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BookCover
+
 
 # Register your models here.
+@admin.register(BookCover)
+class BookCoverAdmin(SummernoteModelAdmin):
+    summernote_fields = ('booking_intro',)
--- book/models.py
+++ book/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class BookCover(models.Model):
+    #The cover image for the booking page
+    cover_image = CloudinaryField('image', default='placeholder')
+    booking_intro = TextField(max_length=200, nullable=False)
+
+    def __str__(self):
+        #Represent
+        return self.booking_intro
