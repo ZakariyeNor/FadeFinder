49886a4b4b0ec060760b1b81b56f433a7ecf8fa2
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-31 08:51:17 +0000

    Colloect static files, install whitenoise, create runtime.txt file and added the later version than the one I using, then prepare for deployment

--- fadefinder/settings.py
+++ fadefinder/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zakariyenor-fadefinder-6wk0ajb85v8.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -57,6 +57,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
