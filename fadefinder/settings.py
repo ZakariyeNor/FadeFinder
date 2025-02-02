73254ac7efbf217ececbc584bbeb24d65161db83
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-02 13:37:55 +0000

    collect static files

--- fadefinder/settings.py
+++ fadefinder/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-zakariyenor-fadefinder-6wk0ajb85v8.ws.codeinstitute-ide.net','.herokuapp.com']
 
