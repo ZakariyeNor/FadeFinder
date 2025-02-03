329376afeb6fa29fe01bf1db581bd8bfdf0a560f
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-03 15:46:24 +0000

    create view for the booking form

--- book/views.py
+++ book/views.py
@@ -11,7 +11,27 @@ class BookCoverShow(generic.ListView):
     context_object_name = 'bookingcover'
 
 
+#View for the booking form
+def booking_view(request):
+    if request.method == "POST":
+        booking_form = BookingForm(data=request.POST)
+        if booking_form.is_valid():
+            booking_form.save()
+            messages.add_message(request, messages.SUCCESS,
+            'You booked an appointment successfully!')
+            return redirect('booking.html')
 
+        else:
+            messages.add_message(request, messages.ERROR,
+            "Couldn't book an appointment. Please try again.")
+    else:
+        booking_form = BookingForm()
+        
+    return render(
+        request,
+        "book/booking.html",
+        {"booking_form": booking_form}, 
+    )
 
 
 
