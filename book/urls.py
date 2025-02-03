bc905e0ea28e604d92e0c7f55379333e68c67dcd
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-03 17:25:16 +0000

    collectstatic files

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,5 @@ from . import views
 #Booking app path
 urlpatterns = [
     path('', views.BookCoverShow.as_view(), name='book'),
-    path('booking', views.booking_view, name='booking_form'),
+    path('book/', views.booking_view, name='booking_form'),
 ]
