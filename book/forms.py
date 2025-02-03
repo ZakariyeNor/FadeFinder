23cf98ec4f3f9531cf86d122eb41c7d19517dba9
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-03 15:29:43 +0000

    give the form logic to check if user choose the barber and service

--- book/forms.py
+++ book/forms.py
@@ -7,4 +7,20 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['barber', 'service']
 
-        
+    def __init__(self, *args, **kwargs):
+        super.__init__(*args, **kwargs)
+
+        #populate services based on the selected barber
+        if 'barber' in self.data:
+            barber_id = self.data.get('barber')
+            self.fields['service'].queryset = BarberService.objects.filter(barber_id=barber_id)
+        else:
+            #Don't show services until user chooses the barber
+            self.fields['service'].queryset = BarberService.objects.none()
+
+    def cleaned_data(self):
+        service = self.cleaned_data.get('service')
+        if not service:
+            raise forms.ValidationError('You need to select a service.')
+        else:
+            return service
--- book/views.py
+++ book/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from .models import BookCover
+from django.contrib import messages
+from .models import BookCover, Booking
+from .forms import BookingForm
 
 # Create your views here.
 class BookCoverShow(generic.ListView):
     model = BookCover
     template_name = 'book/booking.html'
     context_object_name = 'bookingcover'
+
+
+
+
 
 
