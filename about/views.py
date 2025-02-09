5be8660386f1d4ac66f02c4cffec2bc5b9c7cb52
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-09 17:29:34 +0000

    add print statements into about me views

--- about/views.py
+++ about/views.py
@@ -6,6 +6,7 @@ from .forms import AboutForm
 # Create your views here.
 def about_me(request):
     abouts = About.objects.all()
+    print(f"Total about entries: {abouts}")
     form = AboutForm()
 
     about_info = None
@@ -13,12 +14,18 @@ def about_me(request):
 
     if request.method == "POST":
         barber_id = request.POST.get('barber_id')
+        print(f"Received POST request with barber_id: {barber_id}")
     else:
         barber_id = None
+        print("No post data received")
 
     if barber_id:
         barber = get_object_or_404(Barber, id=barber_id)
+        print(f"Selected Barber: {barber.barber_name}")
         about_info = About.objects.filter(about_barber=barber).first()
+        print(f"Found about info: {about_info.title if about_info else 'No About info found for this barber' }")
+    else:
+        print("No barber Id was provided")
 
 
     return render(
