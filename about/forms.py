809745e00cb9654372272a5989d7633314990257
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-10 12:01:28 +0000

    crete collaboration form

---
+++ about/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from .models import Collaboration
+
+class CollaborationForm(forms.ModelForm):
+    class Meta:
+        model = Collaboration
+        fields = ('barber_name', 'barber_shop', 'business_type', 'service_offered', 'email', 'number')
+
+    
+    barber_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter Your Name'
+            }
+        )
+    )
+
+
+    barber_shop = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter Barber Shop Name'
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': 'Enter Your Email'
+            }
+        )
+    )
+
+    number = forms.CharField(
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'Enter Your Number'
+            }
+        )
+    )
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        barber_name = cleaned_data.get('barber_name')
+        barber_shop = cleaned_data.get('barber_shop')
+
+        #check if the combination of barber name and barbershop already exists
+        if barber_name and barber_shop:
+            if Collaboration.objects.filter(barber_name=barber_name, barber_shop=barber_shop).exists():
+                self.add_error('barber_shop', 'A collaboration with this barber name and shop already exists.')
+        return cleaned_data
--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import About
+from .models import About, Collaboration
+from .forms import CollaborationForm
 # from book.models import Barber
 # from .forms import AboutForm
 
@@ -8,11 +9,34 @@ def about_me(request):
     abouts = About.objects.all()
     print(f"Total about entries: {abouts}")
 
+    collaborations = Collaboration.objects.all()
+    forms = CollaborationForm()
+
+    if collaborations:
+        total_collaborations = collaborations.count()
+        print(f"Total Collaborations: {total_collaborations}")
+        
+
+        for collaboration in collaborations:
+            print(f"Barber Name: {collaboration.barber_name}")
+            print(f"Barber Shop: {collaboration.barber_shop}")
+            print(f"Business Type: {collaboration.business_type}")
+            print(f"Service Offered: {collaboration.service_offered}")
+            print(f"Email: {collaboration.email}")
+            print(f"Phone Number: {collaboration.number}")
+            print(f"Additional Info: {collaboration.more_info}")
+            print(f"Created On: {collaboration.created_on}")
+            print(f"Updated On: {collaboration.updated_on}")
+            print("-" * 20)
+    else:
+        print("Not found collaborations")
+
 
     return render(
         request,
         "about/about.html",
         {
             "abouts": abouts,
+            "collaborations": collaborations,
         }
         )
