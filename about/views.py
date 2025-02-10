1c406d6cf0ae8ec61317b38a76fad96216b52d62
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-10 14:38:01 +0000

    give logic to the collaboration form

--- about/views.py
+++ about/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import About, Collaboration
 from .forms import CollaborationForm
-# from book.models import Barber
-# from .forms import AboutForm
 
-# Create your views here.
 def about_me(request):
     abouts = About.objects.all()
     print(f"Total about entries: {abouts}")
