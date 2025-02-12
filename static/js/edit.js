4cbd29cd7c01386dd54617b4771f88e415da651b
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-12 19:53:05 +0000

    Manual testing

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,27 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dateInput = document.querySelector("input[name='date']");
     const timeInput = document.querySelector("input[name='time']");
-    const deleteModal = document.getElementById('deleteModal');
 
     if (dateInput) {
-        flatpickr(dateInput, {
+        const datepicker = flatpickr(dateInput, {
             minDate: "today",
             dateFormat: "Y-m-d",
             defaultDate: ["2025-02-12"],
+            onChange: function(selectedDates, dateStr, instance) {
+                updateMinTime(dateStr);
+            }
         });
+
+        function updateMinTime(selectedDate) {
+            if (timeInput) {
+                const now = new Date();
+                const selected = new Date(selectedDate);
+                let minTimeValue = "00:00"; // Default for future dates
+
+                if (selected.toDateString() === now.toDateString()) {
+                    minTimeValue = now.getHours() + ":" + now.getMinutes();
+                }
+
+                timepicker.set("minTime", minTimeValue);
+            }
+        }
     }
 
     if (timeInput) {
-        flatpickr(timeInput, {
+        const timepicker = flatpickr(timeInput, {
             enableTime: true,
             noCalendar: true,
             dateFormat: "H:i",
             minuteIncrement: 30,
             time_24hr: false,
-            defaultDate: "10:00",
-            minTime: new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+            defaultDate: "20:28",
+            minTime: new Date().getHours() + ":" + new Date().getMinutes(),
         });
     }
-    
-    
 });
