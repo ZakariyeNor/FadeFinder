c12ef38dbd7aaae06543e1e6466593395032d74a
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-13 16:50:14 +0000

    add url test

--- home/tests/tests_urls.py
+++ home/tests/tests_urls.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class BarberInfoContentTest(TestCase):
+
+    def test_view_url(self):
+        # The correct URL name should be 'home' as defined in your urls.py
+        url = reverse('home')  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
