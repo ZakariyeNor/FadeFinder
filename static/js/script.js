cc79aa8fc6b1b975d0151e0555ae9a0a319ebca9
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-02-16 11:02:02 +0000

    Give contact us link in the desktop if user is not staff

--- static/js/script.js
+++ static/js/script.js
@@ -86,3 +86,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// Initialize Bootstrap tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
